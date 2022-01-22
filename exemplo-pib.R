79d1222174dd9cf9c71dd182c071a1e62c5dc32d
paulorobertolds <pauloroberto.lds@gmail.com>
2022-01-22 14:12:25 -0400

    página 2

--- exemplo-pib.R
+++ exemplo-pib.R
@@ -30,3 +30,27 @@ fets2014=forecast(ets2014,h=5)
 #modelo ETS(A,Ad,A)
 hw1a=hw(pibbrrlt2,seasonal="additive", damped=TRUE)
 nd_predhw1a<-ts(c(hw1a$mean[1],hw1a$mean[2],hw1a$mean[3]),start=c(2013,1), frequency=4)
+
+hw1a2014 = hw(pibbrrl,seasonal="additive", damped=TRUE)
+fhw1a2014=forecast(hw1a2014,h=5)
+#modelo ETS(A,A,A)
+hw2a=hw(pibbrrlt2,seasonal="additive", damped=FALSE)
+nd_predhw2a<-ts(c(hw2a$mean[1],hw2a$mean[2],hw2a$mean[3]), +
+                  start=c(2013,1), frequency=4)
+hw2a2014 = hw(pibbrrl,seasonal="additive", damped=FALSE)
+fhw2a2014=forecast(hw2a2014,h=5)
+#modelo arima
+aa2=auto.arima(pibbrrlt2)
+nd_preda<-forecast(aa2, h=3)
+arima2014 = auto.arima(pibbrrl)
+fa2014=forecast(arima2014,h=5)
+#modelo sarimax(1,1,0)x(0,0,1)
+sarimax1=auto.arima(pibbrrlt2,xreg=pibmt2)
+nd_predax<-forecast(sarimax1, h=3,xreg=c(0.90,1.22,1.35))
+arimax2014 = auto.arima(pibbrrl,xreg=pibm)
+fax2014=forecast(arimax2014,xreg=c(0.84,1.08,1.47,1.62,1))
+#tabela 2 (descrição dos dados)
+summary(pibbrr)
+sqrt(var(pibbrr))
+summary(diff(log(pibbrr))*10
+
