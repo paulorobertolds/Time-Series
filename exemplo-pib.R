904827341492bc929502ae2d50be5d2f143e8fa9
paulorobertolds <pauloroberto.lds@gmail.com>
2022-01-22 11:04:33 -0400

    carregando os dados

---
+++ exemplo-pib.R
@@ -0,0 +1,6 @@
+#Rotina do R
+
+#ler os dados
+options(digits=4)
+pibbr=read.delim('https://github.com/paulorobertolds/Time-Series/blob/main/igpditri.txt', header= FALSE)
+igpdio=read.delim('https://github.com/paulorobertolds/Time-Series/blob/main/igpditri.txt', header= FALSE)
